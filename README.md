d6c428d772a3d6d6e377ce38e172b0a83e58317b
kaikanwu <wukaikan@gmail.com>
2019-01-21 22:55:39 +0800

    :tada: let's go!

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+
 This is a Chat project based on SpringBoot and Netty!
+
 Updating!
+
+The front-end part: https://github.com/kaikanwu/chat-front
+
---
+++ src/main/java/com/kk/Application.java
@@ -0,0 +1,18 @@
+package com.kk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+// 扫描 所有需要的包, 包含一些自用的工具类包 所在的路径
+@ComponentScan(basePackages= {"com.kk"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
